7fc52e2d256c5552474c5028d4ed0c68411d4df1
online2 <1018596566@qq.com>
2017-07-05 14:27:28 +0800

    添加gif

--- app/src/main/java/com/mrchao/www/customviewdemo/MainActivity.java
+++ app/src/main/java/com/mrchao/www/customviewdemo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.mrchao.www.customviewdemo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,7 +13,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @BindView(R.id.aiLoad)
     AiLoadView mAiLoad;
