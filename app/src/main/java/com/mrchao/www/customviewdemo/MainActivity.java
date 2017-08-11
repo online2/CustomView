c73b5be7bd1b8e6c6e556b8a71acb2f0dffa44ef
Linfc <1018596566@qq.com>
2017-08-11 15:02:58 +0800

    z

--- app/src/main/java/com/mrchao/www/customviewdemo/MainActivity.java
+++ app/src/main/java/com/mrchao/www/customviewdemo/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.mrchao.www.customviewdemo.chartview.HalfMakerPieView;
+import com.mrchao.www.customviewdemo.chartview.TwoPieView;
+import com.mrchao.www.customviewdemo.chartview.bean.TwoPieViewBean;
 import com.mrchao.www.customviewdemo.view.AiLoadView;
 import com.mrchao.www.customviewdemo.view.HuaWeiLoadView;
 import com.mrchao.www.customviewdemo.view.SercherIconView;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,15 +30,35 @@ public class MainActivity extends Activity {
     Button mAlSucc;
     @BindView(R.id.alFauil)
     Button mAlFauil;
+    @BindView(R.id.dashboard_view_6)
+    HalfMakerPieView mDashboardView6;
+    @BindView(R.id.dashboard_view_7)
+    TwoPieView mDashboardView7;
+
+    private ArrayList<TwoPieViewBean> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        data = new ArrayList<>();
+        TwoPieViewBean data1 = new TwoPieViewBean();
+        data1.setTitle("总目标");
+        data1.setValue("158.1(亿元)");
+        data1.setPencet(80);
+        TwoPieViewBean data2 = new TwoPieViewBean();
+        data2.setTitle("完成:35.16%");
+        data2.setValue("55.5(亿元)");
+        data2.setPencet(20);
+        data.add(data1);
+        data.add(data2);
+        mDashboardView7.setDataInfo(data);
     }
 
-    @OnClick({R.id.aiLoad, R.id.alSucc, R.id.alFauil, R.id.huaweiLoad, R.id.serCherLoad})
+    @OnClick({R.id.aiLoad, R.id.alSucc, R.id.alFauil, R.id.huaweiLoad, R.id.serCherLoad,
+            R.id.dashboard_view_6, R.id.dashboard_view_7})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.alSucc:
@@ -47,6 +72,13 @@ public class MainActivity extends Activity {
             case R.id.serCherLoad:
                 mSerCherLoad.startSer();
                 break;
+            case R.id.dashboard_view_6:
+                mDashboardView6.setAngleValue(40, 50, 90);
+                break;
+            case R.id.dashboard_view_7:
+                mDashboardView7.start();
+                break;
         }
     }
+
 }
